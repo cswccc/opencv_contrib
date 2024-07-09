9d5d1951b1ed277942341bb32453fad52a297bad
cswccc <3344767250@qq.com>
2024-07-09 16:17:30 +0800

    update

--- modules/wechat_qbar/demos/cmd/main.cpp
+++ modules/wechat_qbar/demos/cmd/main.cpp
@@ -18,20 +18,7 @@ std::string GetImagePathFromArgs(int argc, char *argv[])
     return DEFAULT_TEST_IMAGE_PATH;
 }
 
-int main(int argc, char *argv[])
-{
-    std::string image_path = GetImagePathFromArgs(argc, argv);
-    cv::Mat grayscale_image = cv::imread(image_path, cv::IMREAD_GRAYSCALE);
-
-    QBar qbar;
-    QBAR_MODE mode;
-    mode.useAI = true;
-    mode.qbar_ml_mode.detection_model_path_ = "models/detector/qbar_det.yml";
-    mode.qbar_ml_mode.super_resolution_model_path_ = "models/sr/qbar_sr.yml";
-    qbar.SetReaders({ONED_BARCODE, QRCODE, PDF417, DATAMATRIX});
-    qbar.Init(mode);
-    //QBAR_RESULT result = qbar.Decode(grayscale_image, width, height);
-    
+void imageInput(QBar &qbar, cv::Mat &grayscale_image) {
     std::vector<QBAR_RESULT> results = qbar.ScanImage(grayscale_image);
     for(int i = 0; i < results.size(); i++)
     {
@@ -46,6 +33,48 @@ int main(int argc, char *argv[])
             std::cout << "Decode failed!" << std::endl;
         }
     }
+}
+
+void cameraInput(QBar &qbar) {
+    int device_id = 0;
+    auto cap = cv::VideoCapture(device_id);
+    int w = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_WIDTH));
+    int h = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_HEIGHT));
+
+    auto tick_meter = cv::TickMeter();
+    cv::Mat frame;
+    while (cv::waitKey(1) < 0) {
+        bool has_frame = cap.read(frame);
+        if (!has_frame) {
+            std:: cout << "No frames grabbed!\n";
+            break;
+        }
+
+        tick_meter.start();
+        std::vector<QBAR_RESULT> results = qbar.ScanImage(grayscale_image);
+        tick_meter.stop();
+
+        cv::imshow("Wechat QBar", frame);
+
+        tick_meter.reset();
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    std::string image_path = GetImagePathFromArgs(argc, argv);
+    cv::Mat grayscale_image = cv::imread(image_path, cv::IMREAD_GRAYSCALE);
+
+    QBar qbar;
+    QBAR_MODE mode;
+    mode.useAI = true;
+    mode.qbar_ml_mode.detection_model_path_ = "models/detector/qbar_det.yml";
+    mode.qbar_ml_mode.super_resolution_model_path_ = "models/sr/qbar_sr.yml";
+    qbar.SetReaders({ONED_BARCODE, QRCODE, PDF417, DATAMATRIX});
+    qbar.Init(mode);
+    //QBAR_RESULT result = qbar.Decode(grayscale_image, width, height);
+    
+    imageInput(qbar, grayscale_image);
 
     return 0;
 }
