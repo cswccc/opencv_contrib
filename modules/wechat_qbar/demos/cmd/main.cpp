844623ca11d5d598535b74114c4fb9d232cfa40b
cswccc <3344767250@qq.com>
2024-07-11 16:33:25 +0800

    update demo

--- modules/wechat_qbar/demos/cmd/main.cpp
+++ modules/wechat_qbar/demos/cmd/main.cpp
@@ -4,30 +4,7 @@
 using namespace cv;
 using namespace cv::QBarAI;
 
-
-// #define DEFAULT_TEST_IMAGE_PATH "/home/cswccc/Work/opencv_contrib/modules/wechat_qbar/demos/common/images/test.jpg"
-#define DEFAULT_TEST_IMAGE_PATH "/Users/caoshiwen/work/opencv_contrib/modules/wechat_qbar/demos/common/images/test_sr.png"
-
-std::string GetImagePathFromArgs(int argc, char *argv[])
-{
-    if (argc >= 2)
-    {
-        return argv[1];
-    }
-    std::cout << "CMD usage: " << argv[0] << " <image-path>" << std::endl;
-    std::cout << "There's is no input image, use default test image: " << DEFAULT_TEST_IMAGE_PATH << std::endl;
-    return DEFAULT_TEST_IMAGE_PATH;
-}
-
 cv::Mat visualize(const cv::Mat& image, std::vector<QBAR_RESULT>& results, float fps = -1.f) {
-    static cv::Scalar box_color{0, 255, 0};
-    static std::vector<cv::Scalar> landmark_color{
-        cv::Scalar(255,   0,   0), // right eye
-        cv::Scalar(  0,   0, 255), // left eye
-        cv::Scalar(  0, 255,   0), // nose tip
-        cv::Scalar(255,   0, 255), // right mouth corner
-        cv::Scalar(  0, 255, 255)  // left mouth corner
-    };
     static cv::Scalar text_color{0, 255, 0};
 
     auto output_image = image.clone();
@@ -37,16 +14,10 @@ cv::Mat visualize(const cv::Mat& image, std::vector<QBAR_RESULT>& results, float
     }
 
     for (int i = 0; i < results.size(); i++) {
-        std::cout << "Type: " << results[i].typeName << std::endl;
-        std::cout << "Data: " << results[i].data << std::endl;
-        std::string info = results[i].data;
-        cout << "points number: " << results[i].points.size() << endl;
-
         int ux = -1, ly = -1, dx = -1, ry = -1;
         for (int j= 0 ; j < results[i].points.size(); j++) {
             cv::Point point(results[i].points[j].x, results[i].points[j].y);
-            cv::circle(output_image, point, 5, landmark_color[i % landmark_color.size()], -1);
-            cout << results[i].points[j].x << ' ' << results[i].points[j].y << endl;
+            cv::circle(output_image, point, 5, cv::Scalar(0, 255, 0), -1);
 
             if (ly == -1 || ly > results[i].points[j].y) {
                 ly = results[i].points[j].y;
@@ -67,58 +38,32 @@ cv::Mat visualize(const cv::Mat& image, std::vector<QBAR_RESULT>& results, float
         int text_y = (ly + ry) / 2 + textSize.height / 2;
 
         cv::putText(output_image, cv::format("%s", results[i].data.c_str()), cv::Point(text_x, text_y), cv::FONT_HERSHEY_DUPLEX, 0.5, text_color, 1);
-        cout << "----" << endl;
     }
 
     return output_image;
 }
 
-void imageInput(QBar &qbar, cv::Mat &image) {
-    cv::Mat grayscale_image;
-    cv::cvtColor(image, grayscale_image, cv::COLOR_BGR2GRAY);
-    
-    std::vector<QBAR_RESULT> results = qbar.ScanImage(grayscale_image);
-
-    auto res_frame = visualize(image, results);
-
-    cv::namedWindow("Wechat QBar", cv::WINDOW_AUTOSIZE);
-    cv::imshow("Wechat QBar", res_frame);
-    cv::waitKey(0);
-}
-
-void cameraInput(QBar &qbar) {
-    int device_id = 0;
-    auto cap = cv::VideoCapture(device_id);
-    int w = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_WIDTH));
-    int h = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_HEIGHT));
-
-    auto tick_meter = cv::TickMeter();
-    cv::Mat frame;
-    while (cv::waitKey(1) < 0) {
-        bool has_frame = cap.read(frame);
-        if (!has_frame) {
-            std:: cout << "No frames grabbed!\n";
-            break;
-        }
-        cv::Mat grayscale_image;
-        cv::cvtColor(frame, grayscale_image, cv::COLOR_BGR2GRAY);
-
-        tick_meter.start();
-        std::vector<QBAR_RESULT> results = qbar.ScanImage(grayscale_image);
-        tick_meter.stop();
-
-        auto res_frame = visualize(frame, results, (float)tick_meter.getFPS());
-
-        cv::imshow("Wechat QBar", res_frame);
-
-        tick_meter.reset();
-    }
-}
-
 int main(int argc, char *argv[])
 {
-    std::string image_path = GetImagePathFromArgs(argc, argv);
-    cv::Mat image = cv::imread(image_path);
+    cv::CommandLineParser parser(argc, argv,
+        "{help  h           |                                   | Print this message}"
+        "{input i           |                                   | Set input to a certain image, omit if using camera}"
+        "{backend b         | opencv                            | Set DNN backend}"
+        "{target t          | cpu                               | Set DNN target}"
+        "{save s            | false                             | Whether to save result image or not}"
+        "{vis v             | false                             | Whether to visualize result image or not}"
+    );
+    if (parser.has("help"))
+    {
+        parser.printMessage();
+        return 0;
+    }
+
+    std::string input_path = parser.get<std::string>("input");
+    std::string backend = parser.get<std::string>("backend");
+    std::string target = parser.get<std::string>("target");
+    bool save_flag = parser.get<bool>("save");
+    bool vis_flag = parser.get<bool>("vis");
 
     QBar qbar;
     QBAR_MODE mode;
@@ -128,8 +73,60 @@ int main(int argc, char *argv[])
     qbar.SetReaders({ONED_BARCODE, QRCODE, PDF417, DATAMATRIX});
     qbar.Init(mode);
     
-    // imageInput(qbar, image);
-    cameraInput(qbar);
+    if (!input_path.empty()) {
+        auto image = cv::imread(input_path);
+
+        cv::Mat grayscale_image;
+        cv::cvtColor(image, grayscale_image, cv::COLOR_BGR2GRAY);
+        
+        std::vector<QBAR_RESULT> results = qbar.ScanImage(grayscale_image);
+
+        std::cout << cv::format("%d codes detected:\n", results.size());
+        for (int i = 0; i < results.size(); i++) {
+            std::cout << "Code " << i << ":\n";
+            std::cout << "Type: " << results[i].typeName << '\n';
+            std::cout << "Data: " << results[i].data << '\n';
+        }
+
+        if (save_flag || vis_flag) {
+            auto res_frame = visualize(image, results);
+            if (save_flag) {
+                std::cout << "Results are saved to result.jpg\n";
+                cv::imwrite("result.jpg", res_frame);
+            }
+            if (vis_flag) {
+                cv::namedWindow("Wechat QBar", cv::WINDOW_AUTOSIZE);
+                cv::imshow("Wechat QBar", res_frame);
+                cv::waitKey(0);
+            }
+        }
+    }
+    else {
+        int device_id = 0;
+        auto cap = cv::VideoCapture(device_id);
+
+        auto tick_meter = cv::TickMeter();
+        cv::Mat frame;
+        while (cv::waitKey(1) < 0) {
+            bool has_frame = cap.read(frame);
+            if (!has_frame) {
+                std:: cout << "No frames grabbed!\n";
+                break;
+            }
+            cv::Mat grayscale_image;
+            cv::cvtColor(frame, grayscale_image, cv::COLOR_BGR2GRAY);
+
+            tick_meter.start();
+            std::vector<QBAR_RESULT> results = qbar.ScanImage(grayscale_image);
+            tick_meter.stop();
+
+            auto res_frame = visualize(frame, results, (float)tick_meter.getFPS());
+
+            cv::imshow("Wechat QBar", res_frame);
+
+            tick_meter.reset();
+        }
+    }
  
     return 0;
 }
